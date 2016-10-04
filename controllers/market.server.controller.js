cb257177b57c18fe3999be073b94f919d5d4e9f9
Timon Davis <timondavis@yahoo.com>
2016-10-04 03:31:05 -0700

    [+] Basic market report in place

--- controllers/market.server.controller.js
+++ controllers/market.server.controller.js
@@ -1,46 +1,18 @@
 var MarketDataModel = require( '../models/market-data.server.model' );
 var CommodityModel = require( '../models/commodity.server.model' );
 
-exports.test = function( req, res ) {
-var commodities = [
+exports.getAll = function ( req, res, next ) {
 
-        { 'name': 'food', 'cost': 243 },
-        { 'name': 'clothing', 'cost': 300 },
-        { 'name': 'metal', 'cost': 376 },
-        { 'name': 'equipment', 'cost': 568 },
-        { 'name': 'plastic', 'cost': 184 },
-        { 'name': 'medical', 'cost': 787},
-        { 'name': 'industrial', 'cost': 733 },
-        { 'name': 'electronics', 'cost': 848 },
-        { 'name': 'heavyMetals', 'cost': 1012 },
-        { 'name': 'luxuryGoods', 'cost': 1228 }
-    ];
+   var query = MarketDataModel.find().populate( 'commodities' );
 
-    CommodityModel.collection.insert( commodities, function( err, commodityModels ) {
-        if ( err ) {
+   query.exec( function( err, response ) {
 
-            console.log( err );
-        }
-        var marketData = new MarketDataModel( {
-            'date': '9/14/3013',
-            'planet' : 'Tomlinson\'s Planet',
-            'region' : 'Human Space',
-            'commodities': commodityModels.insertedIds
-        });
-
-        marketData.save( function ( err, data ) {
-            if ( err ) {
-
-                console.log( err );
-            } else {
-
-                console.log( data );
-            }
-        });
-
-    });
-
-    res.status( 200 ).send( 'Save Request Accepted' );
+       if ( err ) {
+           next( err );
+       } else {
+           res.status( 200 ).json( response );
+       }
+   });
 };
 
 exports.createItem = function( req, res, next ) {
@@ -70,17 +42,3 @@ exports.createItem = function( req, res, next ) {
     res.status(200).send( 'Save Request Accepted' );
 
 };
-
-exports.testRead = function( req, res ) {
-
-    MarketDataModel.find().populate( 'commodities' ).exec( function( err, commodity ) {
-
-        if ( err ) {
-
-            res.status( 500 ).json( { 'status': 'failed' } );
-        }
-
-        res.status( 200 ).send( 'Save Request Accepted' );
-    });
-};
-
