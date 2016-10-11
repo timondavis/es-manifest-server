a76b47366a8a19421ad6e15da60d1dfcbccd6169
Timon Davis <timondavis@yahoo.com>
2016-10-11 04:34:34 -0700

    [+] Query endpoint seems to be working just great

--- controllers/market.server.controller.js
+++ controllers/market.server.controller.js
@@ -1,44 +1,112 @@
 var MarketDataModel = require( '../models/market-data.server.model' );
 var CommodityModel = require( '../models/commodity.server.model' );
 
-exports.getAll = function ( req, res, next ) {
+exports.getMarketData = function ( req, res, next ) {
 
-   var query = MarketDataModel.find().populate( 'commodities' );
+    var sort = extractSort( req.query );
+    var filters = extractFilters( req.query );
 
-   query.exec( function( err, response ) {
+    console.log( filters );
 
-       if ( err ) {
-           next( err );
-       } else {
-           res.status( 200 ).json( response );
-       }
-   });
+    var query = MarketDataModel.find(filters).sort(sort).populate( 'commodities' );
+
+
+    query.exec( function( err, response ) {
+
+        if ( err ) {
+            next( err );
+        } else {
+            res.status( 200 ).json( response );
+        }
+    });
 };
 
 exports.createItem = function( req, res, next ) {
 
-   CommodityModel.collection.insert( req.body.commodities, function( err, commodityModels ) {
-        if ( err ) {
+    CommodityModel.collection.insert( req.body.commodities, function( err, commodityModels ) {
+         if ( err ) {
 
-            console.log( err );
-        }
+             console.log( err );
+         }
 
-        req.body.commodities = commodityModels.insertedIds;
+         req.body.commodities = commodityModels.insertedIds;
 
-        var marketData = new MarketDataModel( req.body );
+         var marketData = new MarketDataModel( req.body );
 
-        marketData.save( function ( err, data ) {
-            if ( err ) {
+         marketData.save( function ( err, data ) {
+             if ( err ) {
 
-                console.log( err );
-            } else {
+                 console.log( err );
+             } else {
 
-                console.log( data );
+                 console.log( data );
+             }
+         });
+
+     });
+
+     res.status(200).send( 'Save Request Accepted' );
+};
+
+var extractSort = function extractSort( queryString ) {
+
+    if ( ! queryString.sort ) return {};
+
+    var sortObject = {};
+    var field      = queryString.sort;
+    delete queryString.sort;
+
+    sortObject[field] =  1;
+
+    if ( queryString.order && isValidOrderString( queryString.order ) ) {
+        sortObject[field] = queryString.order;
+        delete queryString.order;
+    }
+
+    return sortObject;
+};
+
+var isValidOrderString = function isValidOrderString( orderString ) {
+
+    var isValid = false;
+
+    switch( orderString ) {
+
+        case( 'asc' ):
+        case( 'desc' ):
+        case( 'ascending' ):
+        case( 'descending' ):
+        case( '1' ):
+        case( '-1' ):
+            isValid = true;
+            break;
+        default:
+            isValid = false;
+            break;
+    }
+
+    return isValid;
+};
+
+var extractFilters = function extractFilters( filters ) {
+
+    var processedFilters = {};
+
+    for ( var filterName in filters ) {
+
+        if ( filters.hasOwnProperty( filterName ) ) {
+
+            if ( ! isKeyReserved( filterName ) ) {
+                processedFilters[filterName] = new RegExp( filters[filterName], 'i' );
+                delete filters[filterName];
             }
-        });
+        }
+    }
 
-    });
+    return processedFilters;
+};
 
-    res.status(200).send( 'Save Request Accepted' );
+var isKeyReserved = function isKeyReserved( keyName ) {
 
+    return ( keyName == 'sort' || keyName == 'order' );
 };
